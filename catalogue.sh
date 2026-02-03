97e27d034b21cde5d37b95b4a6702ce816b5985f
sammusku <Samyuktha.musku@gmail.com>
2026-02-03 09:49:23 -0600

    common

---
+++ catalogue.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source ./common
+check_root
+app_name=catalogue
+nodejs_setup
+systemd_setup
+
+#Load data into mongodb
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo  &>>$LOGS_FILE
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+
+INDEX=$(mongosh --host "$MONGODB_HOST" --quiet \
+  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -eq -1 ]; then
+   mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOGS_FILE
+   VALIDATE $? "products loading"
+else 
+   echo -e "products already loaded ---$Y skipping $N "
+fi
+
+app_restart
--- common.sh
+++ common.sh
@@ -8,6 +8,8 @@
  Y="\e[33m"
  N="\e[0m"
 START_TIME=$(date +%s)
+SCRIPT_DIR=$PWD
+MONGODB_HOST="mongodb.dev88s.online"
 
 mkdir -p $LOGS_FOLDER
 
@@ -29,7 +31,67 @@ VALIDATE() {
        echo -e "$(date "+%y-%m-%d %H:%M:%S") | $Y $2 $N------$G success $N" | tee -a $LOGS_FILE
     fi
 }
+app_setup(){
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+   VALIDATE $? "Creating system user"
+   echo " $G Created the roboshop user $N "
+else
+   echo -e " roboshop user already exist in the system..$Y skipping $N "
+fi
 
+mkdir -p /app
+VALIDATE $? "creating app directory"
+
+
+curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip  &>>$LOGS_FILE
+VALIDATE $? "Downloading $app_name data"
+
+cd /app
+VALIDATE $? "moving to app directory"
+
+rm -rf /app/*
+VALIDATE $? "removing already existed data from app"
+
+unzip /tmp/$app_name.zip &>>$LOGS_FILE
+VALIDATE $? "unzip $app_name data from temp directory to app directory"
+
+}
+
+nodejs_setup(){
+    dnf module list nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "List of avaialble nodejs versions"
+
+    dnf module disable nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Disable nodejs default  version"
+
+    dnf module enable nodejs:20 -y &>>$LOGS_FILE
+    VALIDATE $? "enable nodejs:20 version"
+
+    dnf install nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "installing Nodejs 20"
+    npm install  &>>$LOGS_FILE
+    VALIDATE $? "installing the nodejs dependencies"
+}
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service &>>$LOGS_FILE
+    VALIDATE $? "copying the system services"
+
+    systemctl daemon-reload
+    VALIDATE $? "reloading the changes"
+
+    systemctl enable $app_name &>>$LOGS_FILE
+    VALIDATE $? "enable $app_name"
+
+    systemctl start $app_name &>>$LOGS_FILE
+    VALIDATE $? "start $app_name server"
+}
+
+app_restart(){
+    systemctl restart catalogue &>>$LOGS_FILE
+    VALIDATE $? "restarting the catalogue"
+}
 print_total_time(){
      END_TIME=$(date +%s)
      TOTAL_TIME=$(( $END_TIME - $START_TIME ))
