fa869e3775d3d2707ac4bfe3d32a9cc3c3e90dec
sammusku <Samyuktha.musku@gmail.com>
2026-02-03 09:03:27 -0600

    common

--- common.sh
+++ common.sh
@@ -23,10 +23,10 @@ check_root(){
 
 VALIDATE() {
       if [ $1 -ne 0 ]; then
-       echo -e $(date "+%y-%m-%d %H:%M:%S") | "$Y $2 $N----- $R failure $N" | tee -a $LOGS_FILE
+       echo -e "$(date "+%y-%m-%d %H:%M:%S") | $Y $2 $N----- $R failure $N" | tee -a $LOGS_FILE
        exit 1
     else
-       echo -e $(date "+%y-%m-%d %H:%M:%S") | "$Y $2 $N------$G success $N" | tee -a $LOGS_FILE
+       echo -e "$(date "+%y-%m-%d %H:%M:%S") | $Y $2 $N------$G success $N" | tee -a $LOGS_FILE
     fi
 }
 
