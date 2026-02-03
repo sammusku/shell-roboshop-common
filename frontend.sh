7e5a8bd5f00b5700d8669f159eb21c66db04f843
sammusku <Samyuktha.musku@gmail.com>
2026-02-03 12:47:04 -0600

    common

---
+++ frontend.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+souce ./common.sh
+
+check_root
+app_setup
+
+dnf module disable nginx -y 
+dnf module enable nginx:1.24 -y &>>$LOGS_FILE
+dnf install nginx -y  &>>$LOGS_FILE
+VALIDATE $? "installing nginx"
+
+systemctl enable nginx 
+systemctl start nginx 
+VALIDATE $? "enable and start nginx"
+
+rm -rf /usr/share/nginx/html/* 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+VALIDATE $? "remove data and download"
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+VALIDATE $? "unzip frontend data"
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf  &>>$LOGS_FILE
+VALIDATE $? "copying nginx configure data"
+
+app_restart
+print_total_time
